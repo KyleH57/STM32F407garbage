a0a8d6cc74693bde53b9b6de4f500a0c4af57d03
KyleH57 <kyle.hashiro@gmail.com>
2021-04-14 22:24:58 -0700

    created more CDC bugs

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -110,17 +110,56 @@ int main(void) {
 	MX_USB_DEVICE_Init();
 	/* USER CODE BEGIN 2 */
 
+	uint8_t tempBuf[32];
+
 	const uint8_t spStat[3] = {'S', 'P','\n'};
 	const uint8_t onStat[3] = {'M', '3','\n'};
 	const uint8_t offStat[3] = {'M', '5','\n'};
-	//uint8_t txBuf[8] = { 'A', '2', '3', '4', '5', '6', '7', '\n' };
-	uint8_t rxBuf[8] = { '0', '0', '0', '0', '0', '0', '0', '\n' };
-	//string *rxPtr1 = &rxBuf[1];
-	char *rxPtr1 = &rxBuf[1];
 
-	uint8_t CDCrx[8] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', '\n'};
+	//uint8_t txBuf[8] = { 'A', '2', '3', '4', '5', '6', '7', '\n' };
+	uint8_t rxBuf[16]; // = { '0', '0', '0', '0', '0', '0', '0', '\n', '\0' };
+
+
+	uint8_t CDCrx[8] = { '0', '0', '0', '0', '0', '0', '0', '\n' };
+
+	char *CDCrxPtr1 = &CDCrx[1];
+
+	uint32_t x = -99;
+	uint16_t length, length2;
+
+    char *tempPtr = &x;
 
 	int rpm = 0;
+    int j = 00;
+
+    tempPtr = rxBuf;
+    strcpy(rxBuf, "1234ABCD");
+
+//	while (j > 0) {
+//
+////	    sprintf(tempBuf, "L[%d] = %d", j, x);
+//	    sprintf(tempBuf, "L%d-", j);
+//		length = strlen(tempBuf);
+//
+//		CDC_Transmit_FS(tempBuf, length);
+//		HAL_Delay(400);
+//
+//		length2 = strlen(rxBuf);
+//		CDC_Transmit_FS(rxBuf, length2);
+//		HAL_Delay(400);
+//
+//	    sprintf(tempBuf, "-R%d=%d", j, length2);
+//
+//	    length = strlen(tempBuf);
+//		CDC_Transmit_FS(tempBuf, length);
+//
+//
+//		j--;
+//		HAL_Delay(400);
+//	}
+
+
+
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
@@ -130,48 +169,49 @@ int main(void) {
 
 		/* USER CODE BEGIN 3 */
 
-		HAL_Delay(100);
+
 
 		//HAL_Delay(3);
 
-		hStatus = HAL_UART_Receive(&huart3, rxBuf, 8, 10);
+		//hStatus = HAL_UART_Receive(&huart3, rxBuf, 8, 10);
 
 		//HAL_UART_Receive(&huart3, rxBuf, 8, 10);
 
-		uint32_t x = 0;
 
 		CDC_Receive_FS(CDCrx, &x);
 
+		HAL_Delay(1000);
+
 		if (CDCrx[0] == 'M') {
-			if (CDCrx[1] == '3') {
-				//HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
-				spindleFWD(&huart3);
-				CDC_Transmit_FS(onStat, 3);
-			} else if (CDCrx[1] == '5') {
-				//HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
-				spindleOff(&huart3);
-				CDC_Transmit_FS(offStat, 3);
-			}
+//			if (CDCrx[1] == '3') {
+//				//HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
+//				spindleFWD(&huart3);
+//				CDC_Transmit_FS(onStat, 3);
+//			} else if (CDCrx[1] == '5') {
+//				//HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+//				spindleOff(&huart3);
+//				CDC_Transmit_FS(offStat, 3);
+//			}
 
 		}
 		else if (CDCrx[0] == 'S') {
-			CDC_Transmit_FS(CDCrx, 8);
-			for (int i = 0; i < 6; i++) {
-				if (CDCrx[i] == '\n') {
-
-					//set newline to null so we can use stoi
-					CDCrx[i] = 'X';
-					break;
-				}
-			}
-
+			HAL_Delay(100);
+//			for (int i = 0; i < 6; i++) {
+//				if (CDCrx[i] == 10) {
+//
+//					//set newline to null so we can use stoi
+//
+//					HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
+//					break;
+//				}
+//			}
+			//CDCrx[6] = NULL;
 			//rounddown ok
-			rpm = atoi(rxPtr1)/3;
+			rpm = atoi(CDCrxPtr1)/3;
 			setFreq(rpm, &huart3);
 			CDC_Transmit_FS(CDCrx, 8);
 		} else {
 
-			//HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
 		}
 
 		CDCrx[0] = 'a';
@@ -184,7 +224,6 @@ int main(void) {
 		{
 			HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
 		}
-		//CDC_Transmit_FS(CDCrx, 8);
 
 	}
 	/* USER CODE END 3 */
