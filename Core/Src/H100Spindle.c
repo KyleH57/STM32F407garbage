62139f21c5cdd8945e0f15a33c8d2fdb30b69c33
Kyle H <kyle.hashiro@gmail.com>
2023-01-12 22:35:06 -0800

    stuff

--- Core/Src/H100Spindle.c
+++ Core/Src/H100Spindle.c
@@ -42,7 +42,7 @@ int H100SetRPM(UART_HandleTypeDef *uart, int RPM)
 	 * For example, 0x9C40 = 40,000 = 40% = 160Hz for a 400Hz motor and 320Hz for a 800Hz motor
 	 */
 	float percentRPM = RPM / 24000.0; //max RPM = 24000
-	uint16_t RPM_data= (int)(percentRPM * 100000); //convert to H100 format
+	uint16_t RPM_data = (int)(percentRPM * 10000); //convert to H100 format
 
 	//uint8_t testBuff[5];
 	//testBuff[0] = RPM_data >> 8;..
